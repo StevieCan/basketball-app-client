2b8edf5e27edd65d33b6cec99cc3e4d992eba8d1
Steve Han <shan61489@gmail.com>
2019-01-08 10:23:55 -0600

    client authentication complete

---
+++ app/controllers/client/teams_controller.rb
@@ -0,0 +1,2 @@
+class Client::TeamsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
+  
   namespace :client do
     get '/players' => 'players#index'
     get '/players/new' => 'players#new'
