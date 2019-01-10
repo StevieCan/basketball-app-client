c4b964b17cbed02a5b5c4088e8ba2033cd5b009a
Steve Han <shan61489@gmail.com>
2019-01-10 11:49:23 -0600

    added player controller actions

--- app/controllers/client/players_controller.rb
+++ app/controllers/client/players_controller.rb
@@ -21,4 +21,40 @@ class Client::PlayersController < ApplicationController
     render 'new.html.erb'
   end
 
+  def create
+    client_params = {
+                    name: params[:name],
+                    position: params[:position],
+                    age: params[:age],
+                    points: params[:points],
+                    team: params[:team]
+                    }
+    response = Unirest.post(
+                            "http://localhost:3000/api/players",
+                            parameters: client_params
+                            )
+    if response code == 200
+      session[:player_id] = response.body["id"]
+      flash[:success] = 'Successfully created player!'
+      redirect to '/login'
+    else
+      flash[:warning] = 'Invalid email or password!'
+      redirect_to 'client/signup'
+    end
+  end
+
+  def show
+    player_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/players/#{player_id}")
+    @player = resonse.body
+    render 'show.html.erb'
+  end
+
+  def destroy
+    player_id = params[:id]
+    response = Unirest.delete("http://localhost:3000/api/players/#{player_id}")
+    redirect_to "/client/players"
+  end
+
+
 end
