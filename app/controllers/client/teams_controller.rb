4cb087adf7773b8290a5d57bf330b6cf7c494379
Steve Han <shan61489@gmail.com>
2019-01-12 11:48:27 -0600

    added team controller actions and new view

--- app/controllers/client/teams_controller.rb
+++ app/controllers/client/teams_controller.rb
@@ -18,5 +18,41 @@ class Client::TeamsController < ApplicationController
   def new
     @team = {}
     render 'new.html.erb'    
-  end  
+  end 
+
+  def create
+    client_params = {
+                    name: params[:name],
+                    points_allowed: params[:points_allowed],
+                    points_scored: params[:points_scored],
+                    avg_age: params[:avg_age]
+                    }
+    response = Unirest.post(
+                            "http://localhost:3000/api/teams",
+                            parameters: client_params
+                            )
+    if response code == 200
+      session[:team_id] = response.body["id"]
+      flash[:success] = "Successfully created team!"
+      redirect to '/login'
+    else
+      flash[:warning] = 'Invalid email or password!'
+      redirect_to 'client/signup'
+    end
+  end
+
+  def show
+    team_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/teams/#{team_id}")
+    @team = response.body
+    render 'show.html.erb'
+  end
+
+  def destroy
+    team_id = params[:id]
+    response = Unirest.delete("http://localhost:3000/api/teams/#{team_id}")
+    redirect_to "/client/teams"
+  end
+
+
 end
